aca63ad9dccf150d885cdfdd1841aab3c110bda3
kouamax15 <kouamax15@gmail.com>
2019-08-23 14:55:59 -0400

    bullhorn final project

---
+++ src/main/java/com/example/demo/BasicBullhorn1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicBullhorn1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicBullhorn1Application.class, args);
+    }
+
+}
